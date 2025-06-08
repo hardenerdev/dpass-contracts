2fb231faa0f2d0acec6cdb4fee9ba06282b5d3e6
hardenerdev <hardenerdev@gmail.com>
2025-06-09 00:51:48 +0200

    feat: nft burnable extension

--- contracts/DpassNFT.sol
+++ contracts/DpassNFT.sol
@@ -2,11 +2,12 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.27;
 
-import {ERC721} from '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import {ERC721Pausable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DpassNFT is ERC721, ERC721Pausable, Ownable {
+contract DpassNFT is ERC721, ERC721Pausable, Ownable, ERC721Burnable {
     uint256 private _nextTokenId;
 
     constructor(address initialOwner)
